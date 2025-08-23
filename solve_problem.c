f78feb5708749b277f2e242b8705e09d41ad9bdb
HasibCoderLab <mohammodhasibhasan@gmail.com>
2025-08-23 22:28:55 +0600

    write a code for 2nd student

--- solve_problem.c
+++ solve_problem.c
@@ -15,11 +15,23 @@ int roll;
 float cgpa;
 };
 int main(){
+    // 1st Student 
     struct student s1 ;
     strcpy(s1.name , "Coder");
     s1.roll = 101;
     s1.cgpa = 3.6;
-    
+    printf("student name = %s\n" , s1.name);
+    printf("Student roll no : %d\n", s1.roll);
+    printf("Student cgpa : %f\n" , s1.cgpa);
+    // 2d Student 
+    struct student s2;
+    strcpy(s2.name , "Programmer");
+    s2.roll = 103;
+    s2.cgpa = 3.4;
+    printf("Student Name %s\n" , s2.name);
+    printf("student roll no : %d\n" , s2.roll);
+    printf("Student cgpa :%f\n" , s2.cgpa);
+
     return 0;
 }
 
